577fb62b713fe7bd5708ac32013aa18b41b59784
Greg Lewis <glewis@eyesbeyond.com>
2023-11-18 17:03:07 -0800

    Fix the Zero build
    * Use the same method as other architectures for finding the stack
      bottom and size

--- src/hotspot/os_cpu/bsd_zero/os_bsd_zero.cpp
+++ src/hotspot/os_cpu/bsd_zero/os_bsd_zero.cpp
@@ -297,7 +297,8 @@ void os::current_stack_base_and_size(address* base, size_t* size) {
   if (rslt != 0)
     fatal("pthread_attr_get_np failed with error = %d", rslt);
 
-  if (pthread_attr_getstack(&attr, (void **) &stack_bottom, &stack_bytes) != 0) {
+  if (pthread_attr_getstackaddr(&attr, (void **)&bottom) != 0 ||
+      pthread_attr_getstacksize(&attr, size) != 0) {
     fatal("Can not locate current stack attributes!");
   }
 
